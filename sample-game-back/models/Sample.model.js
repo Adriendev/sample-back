212ff8bcc01668a9c560ed0a037e83d14ea3192b
Adrien Deveaux <adrien.dvx@gmail.com>
2022-08-08 10:54:22 +0200

    changing plans on the models and fixing the seed files

--- sample-game-back/models/Sample.model.js
+++ sample-game-back/models/Sample.model.js
@@ -1,11 +1,21 @@
 const { Schema, model } = require("mongoose");
 
 const sampleSchema = new Schema({
+  sample :{
   title: String,
-  artiste: String,
+  artist: String,
   genre: [String],
   timecode: String,
   url: String,
+  },
+  music :{
+    title: String,
+    artist: String,
+    genre: [String],
+    timecode: String,
+    url: String,
+  },
+  hasBeenPLayed: Boolean,
 });
 
 const Sample = model("Sample", sampleSchema);
