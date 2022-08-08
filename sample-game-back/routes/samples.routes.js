d6746f4ef8be067b17371f34b2eaae933333e4db
Adrien Deveaux <adrien.dvx@gmail.com>
2022-08-08 18:19:22 +0200

    fixing route and testing GET route on Postman

--- sample-game-back/routes/samples.routes.js
+++ sample-game-back/routes/samples.routes.js
@@ -1,10 +1,13 @@
 const Sample = require("../models/Sample.model");
+const router = require("express").Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const samples = await Sample.find();
-    res.status(200).json({ samples });
+    res.status(200).json(samples);
   } catch (err) {
     next(err);
   }
 });
+
+module.exports = router;
--- sample-game-back/utils/consts.js
+++ sample-game-back/utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/sample-game-back";
+const MONGO_URI = "mongodb://localhost/sample-game-back" || process.env.MONGODB_URI ;
 
 module.exports = MONGO_URI;
